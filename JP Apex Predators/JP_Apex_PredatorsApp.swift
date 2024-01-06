ae25e2b8b8345ced908cb8719ed84de94e29cb4e
Rafael Almeida <rafaelpbcp@gmail.com>
2024-01-05 19:47:33 -0500

    Initial Commit

---
+++ JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by Rafael Almeida on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
